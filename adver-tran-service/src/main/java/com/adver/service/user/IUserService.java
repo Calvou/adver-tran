865e2cee8e6252d0693e92f1c5b49c982f0786c6
liguangfa <ligfa2006@126.com>
2018-07-16 23:02:09 +0800

    init delete

--- adver-tran-service/src/main/java/com/adver/service/user/IUserService.java
+++
@@ -1,7 +0,0 @@
-package com.adver.service.user;
-
-public interface IUserService {
-
-	public int saveUser() throws Exception;
-	
-}
--- adver-tran-web/src/main/java/com/adver/web/conf/WebConf.java
+++
@@ -1,6 +0,0 @@
-package com.adver.web.conf;
-
-
-public class WebConf {
-
-}
